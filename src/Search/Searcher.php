a1c3320a7d0f066a94724de3cff7ba66dede21b3
bashyauri <80903696+bashyauri@users.noreply.github.com>
2022-12-15 23:52:54 +0100

    Fine tuning

--- src/Search/Searcher.php
+++ src/Search/Searcher.php
@@ -6,28 +6,36 @@ class Searcher
 {
     public $available_rooms = array();
     public $roomsFound = array();
-     
+   
 
     public function add(array $property): void
     {
+       $count= 0;
       
        
         for ($i=0; $i < count($property); $i++) { 
            
-           // filter only rooms that are available     
+           // filter only rooms that are available    
+          
+            
             if ($property[$i]['Available']){
-                $this->available_rooms[$i]['id'] = $property[$i]['id'];
-                $this->available_rooms[$i]['Name'] = $property[$i]['Name'];
-                $this->available_room[$i]['Available'] =$property[$i]['Available'];;
-                $this->available_rooms[$i]['Floor'] = $property[$i]['Floor'];
-                $this->available_room[$i]['Room No'] = $property[$i]['Room No'];
-                $this->available_rooms[$i]['Per Room Price'] = $property[$i]['Per Room Price'];
-                // echo $this->available_rooms[$i]['Name'].' '. $this->available_rooms[$i]['Per Room Price'].' '.$this->available_rooms[$i]['Floor'];
+                
+               
+                $this->available_rooms[$count]['id'] = $property[$i]['id'];
+                $this->available_rooms[$count]['Name'] = $property[$i]['Name'];
+                $this->available_rooms[$count]['Available'] = $property[$i]['Available'];
+               
+                $this->available_rooms[$count]['Floor'] = $property[$i]['Floor'];
+                $this->available_rooms[$count]['Room No'] = $property[$i]['Room No'];
+                $this->available_rooms[$count]['Per Room Price'] = $property[$i]['Per Room Price'];
+                // echo $property[$i]['Room No'].' '. $this->available_rooms[$i]['Per Room Price'].' '.$this->available_rooms[$i]['Floor'];
                 // echo "\n";
+                $count++;
                 
             }
           
         }
+       
       
        
       
@@ -37,7 +45,9 @@ class Searcher
     
     public function search(int $roomsRequired, $minimum, $maximum):array
     {
+       
         $count = 0;
+        
       
         $rooms = array(); 
         // insert only rooms that are available into an array
@@ -46,15 +56,50 @@ class Searcher
           $count+=1;
         }
         $counter= 0;
+    
         // filter on rooms that are within the price specified
-       while ($counter < count($this->roomsFound)) { 
+       while ($counter < count($this->roomsFound) ) { 
+        
         if ( $this->roomsFound[$counter]['Per Room Price'] >= $minimum  && $this->roomsFound[$counter]['Per Room Price'] <= $maximum){
-            $rooms[$counter]['Name'] = $this->roomsFound[$counter]['Name'];
-            $rooms[$counter]['Floor'] = $this->roomsFound[$counter]['Floor'];
-            $rooms[$counter]['Per Room Price'] = $this->roomsFound[$counter]['Per Room Price'];
-           
+               
+                 
+               
+        if ($roomsRequired > 1){
+            if(isset($this->roomsFound[$counter]['Room No']))
+            {
+                $last_room = $this->roomsFound[$counter]['Room No'];
+                $prev_floor = $this->roomsFound[$counter]['Floor'];
+            
+            }
+            if(isset($this->roomsFound[$counter + 1]['Room No'])){
+        
+                $current_room = $this->roomsFound[$counter +1]['Room No'];
+                $current_floor = $this->roomsFound[$counter]['Floor'];
+        
+            } 
+            if ($last_room < $current_room  &&  $prev_floor == $current_floor )
+            {
+          
+                $rooms[$counter]['Name'] = $this->roomsFound[$counter]['Name'];
+                $rooms[$counter]['Available'] = $this->roomsFound[$counter]['Available'];
+                $rooms[$counter]['Floor'] = $this->roomsFound[$counter]['Floor'];
+                $rooms[$counter]['Room No'] = $this->roomsFound[$counter]['Room No'];
+                $rooms[$counter]['Per Room Price'] = $this->roomsFound[$counter]['Per Room Price'];
+            }
         }
-        $counter+=1;   
+    else
+    {
+        $rooms[$counter]['Name'] = $this->roomsFound[$counter]['Name'];
+                $rooms[$counter]['Available'] = $this->roomsFound[$counter]['Available'];
+                $rooms[$counter]['Floor'] = $this->roomsFound[$counter]['Floor'];
+                $rooms[$counter]['Room No'] = $this->roomsFound[$counter]['Room No'];
+                $rooms[$counter]['Per Room Price'] = $this->roomsFound[$counter]['Per Room Price'];
+    }
+              
+        }
+        
+        $counter+=1;
+       
            
        }
       
@@ -82,10 +127,30 @@ $hotels = [
     ['id'=>14,'Name'=>'Hostel B','Available'=>False,'Floor'=>2,'Room No'=>7,'Per Room Price'=>49.80]];
 $search =new Searcher();
 $search->add($hotels);
-$rooms = $search->search(2,25,40);
+
+$criteria1 = $search->search(2,20,30);
 
 
-foreach ($rooms as $key => $value) {
-    echo $rooms[$key]['Name'].' '.$rooms[$key]['Floor'].' '.$rooms[$key]['Per Room Price']."\n";
+foreach ($criteria1 as $key => $value) {
+    echo $criteria1[$key]['Name'].' '.$criteria1[$key]['Floor'].' '.$criteria1[$key]['Per Room Price']."\n";
+    
+}
+
+
+
+print("\n");
+print("Criteria Two\n");
+
+$criteria2 = $search->search(2,30,50);
+foreach ($criteria2 as $key => $value) {
+    echo $criteria2[$key]['Name'].' '.$criteria2[$key]['Floor'].' '.$criteria2[$key]['Per Room Price']."\n";
+    
+}
+print("\n");
+print("Criteria Three\n");
+
+$criteria3 = $search->search(1,25,40);
+foreach ($criteria3 as $key => $value) {
+    echo $criteria3[$key]['Name'].' '.$criteria3[$key]['Floor'].' '.$criteria3[$key]['Per Room Price']."\n";
     
 }
